d9cdbe38e699057009165c9a3865787836051994
SumitGITT <37958574+SumitGITT@users.noreply.github.com>
2018-04-07 18:44:31 +0530

    Update createbucket.php

--- createbucket.php
+++ createbucket.php
@@ -1,5 +1,6 @@
-<?php
-//copyright 2015 - A Cloud Guru.
+<?php  
+/*
+//copyright 2018 SumitGitt.
 
 //connection string
 include 'connecttoaws.php';
@@ -17,4 +18,23 @@ echo "<h1 align=\"center\">Hello Cloud Guru!</h1>";
 echo "<div align = \"center\"><img src=\"https://acloud.guru/images/logo-small-optimised.png\"></img></div>";
 echo "<h2 align=\"center\">You have successfully created a bucket called {$bucket}</h2>";
 echo "<div align=\"center\"><a href=\"createfile.php?bucket=$bucket\">Click Here to Continue</a></div>";
+
+*/
+?>
+
+
+<?php
+//copyright SumitGitt 2018
+
+//connection string
+include 'connectiontoaws.php'
+
+//create a bucket name
+    $bucketname= uniqid("testid", true);
+
+//create bucket using bucket name
+    $bucket= $client->
+    
+
+
 ?>
