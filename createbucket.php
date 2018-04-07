3c7f2334fe88392953ab60bc6a96339782549fde
SumitGITT <37958574+SumitGITT@users.noreply.github.com>
2018-04-07 21:01:49 +0530

    Update createbucket.php

--- createbucket.php
+++ createbucket.php
@@ -41,6 +41,6 @@ echo "<h1 align=\"center\">Hello Guys</h1>";
 echo "<div align=\"center\"><img src=\"https://scontent-bom1-1.xx.fbcdn.net/v/t1.0-9/14225511_1070192283077863_5437397168006365130_n.jpg?_nc_cat=0&oh=9fb8ca5ab7b32c2e41e3b354053fdfd1&oe=5B305F88\"></img></div>";
 echo "<h2 align=\"center\"> You have successfully created the bucket [$bucketname]</h2>";
 echo "<h2 align=\"center\"><a href=\"createfile.php?bucket=$bucket\">Click here to continue</a></h2>";
-    
+     
 
 ?>
